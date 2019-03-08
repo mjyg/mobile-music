dc5e6d87807a8e3d52dfadeb3e5d6a3c5df1dc66
Jie <940095681@qq.com>
2019-03-08 17:40:15 +0800

    配置proxyTable，抓取歌手详情数据

--- src/api/singer.js
+++ src/api/singer.js
@@ -1,7 +1,5 @@
-import {commonParams} from './config'
 import axios from 'axios'
-import jsonp from 'common/js/jsonp'
-import {commonParams, options} from './config'
+import {commonParams} from './config'
 
 export function getSingerList() {
   const url = '/api/getSingerList'
@@ -12,40 +10,23 @@ export function getSingerList() {
   })
 }
 
-// export function getSingerDetail(singerId) {
-//   const url = '/getSingerDetail'
-//   const data = Object.assign({}, commonParams, {
-//     loginUin: 0,
-//     hostUin: 0,
-//     format: 'json',
-//     platform: 'yqq.json',
-//     needNewCode: 0,
-//     ct: 24,
-//     order: 'listen',
-//     begin: 0,
-//     num: 80,
-//     singermid: singerId
-//   })
-//   return axios.get(url, {
-//     params: data
-//   }).then((res) => {
-//     return Promise.resolve(res.data)
-//   })
-// }
 export function getSingerDetail(singerId) {
-  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
-
+  const url = '/getSingerDetail'
   const data = Object.assign({}, commonParams, {
+    loginUin: 0,
     hostUin: 0,
+    format: 'json',
+    platform: 'yqq.json',
     needNewCode: 0,
-    platform: 'yqq',
+    ct: 24,
     order: 'listen',
     begin: 0,
     num: 80,
-    songstatus: 1,
     singermid: singerId
   })
-
-  return jsonp(url, data, options)
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
 }
-
