a40ac8e31308fc4cd697727ec608b709a5d190f6
Jie <940095681@qq.com>
2019-03-11 14:38:48 +0800

    定义播放器的全局数据

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,19 @@
 // 有时候我们需要从 store 中的 state 中派生出一些状态，例如对列表进行过滤并计数。减少我们对这些状态数据的操作
 // 简单粗暴理解：状态树上的数据比较复杂了，我们使用的时候要简化操作，
 export const singer = state => state.singer
+
+export const playing = state => state.playing
+
+export const fullScreen = state => state.fullScreen
+
+export const playlist = state => state.playlist
+
+export const sequenceList = state => state.sequenceList
+
+export const mode = state => state.mode
+
+export const currentIndex = state => state.currentIndex
+
+export const currentSong = (state) => {
+  return state.playlist[state.currentIndex] || {}
+}
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,12 @@
 // 定义状态
+import {playMode} from 'common/js/config'
 
 export const state = {
-  singer: {}
+  singer: {},
+  playing: false,
+  fullScreen: false,
+  playlist: [],
+  sequenceList: [],
+  mode: playMode.sequence,
+  currentIndex: -1
 }
